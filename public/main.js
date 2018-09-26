f99242704536a345e298240e22555826adbc9ae6
long <long11.nguyenduc@nttdata.com>
2018-09-26 17:30:06 +0700

    update turn server

--- public/main.js
+++ public/main.js
@@ -25,9 +25,9 @@ app.controller('myCtrl', function($scope) {
     var configuration = {"iceServers": [
         {"url": "stun:stun.l.google.com:19302"},
         {
-        	"url": 'turn:numb.viagenie.ca',
-        	"credential": 'muazkh',
-        	"username": 'webrtc@live.com'
+        	"url": 'turn:192.158.29.39:3478?transport=tcp',
+        	"credential": 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        	"username": '28224511:1379330808'
         }
     ]};
 

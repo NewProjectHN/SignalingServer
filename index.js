b3779d1a6c504caab11856c4aa6b182dc220b143
Long <nguyenduclong02@gmail.com>
2018-11-18 11:14:10 +0700

    asdf

---
+++ index.js
@@ -0,0 +1,343 @@
+// Setup basic express server
+// var sslRedirect = require('heroku-ssl-redirect');
+var express = require('express');
+var app = express();
+// app.use(sslRedirect());
+var path = require('path');
+
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+app.use(cors());
+app.options('*', cors());
+
+var socketioAuth = require('./lib/socket-auth.js');
+var userService = require('./lib/user-service.js');
+var socketIORoom = require('./lib/socket-room.js');
+var socketRTC = require('./lib/socket-rtc.js');
+
+var fs = require('fs');
+var httpsOptions = {
+ key: fs.readFileSync('./fake-keys/privateKey.key'),
+ cert: fs.readFileSync('./fake-keys/certificate.crt')
+};
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// console.log(httpsOptions);
+// var port = process.env.PORT || 3000;
+// var server = require('https').createServer(app);
+// var io = require('socket.io')(server);
+// OPen C:\OpenSSL\bin\openssl
+// Genkey with this command: req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout privateKey.key -out certificate.crt
+// app.enable('trust proxy');
+// var server = require('https').createServer(httpsOptions,app);
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+var router = express.Router();
+
+// Map of token to valid user for testing only
+// var mapToken = {}
+app.use('/api', router);
+var userOnlineList = [];//{userId:1,socketId:2,userType:0}.0:Benh nhan, 1:bacsy
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// function getUserBySocketId(socketId){
+//   var token = mapSocketIdToken[socketId];
+//   var user = mapToken[token];
+//   return getUserActiveByUser(user);
+// }
+//
+function getUserActiveByUser(user){
+  var userOnline = null;
+  if(user){
+    for(var i = 0 ;i < userOnlineList.length;i++){
+      var userActive = userOnlineList[i];
+       if(userActive.userId == user.userId && userActive.userType == user.userType){
+         userOnline = userActive;
+         break;
+       }
+    }
+  }
+  return userOnline;
+}
+
+// router.post('/checkLogin', function(req, res) {
+//     var username = req.body.username;
+//     var password = req.body.password;
+//     var token = userService.doCheckLogin(username,password);
+//     var user = {userId:username};
+//
+//     var userActive = getUserActiveByUser({userId:username});
+//     if(userActive != null){
+//         res.json({ status: 'connected',token: token });
+//     }else{
+//         mapToken[token] = user;
+//         res.json({ status: 'OK',token: token });
+//     }
+//
+// });
+//
+
+
+server.listen(port, () => {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
+//app.use(express.static(path.join(__dirname, 'public')));
+
+// Chatroom
+var numUsers = 0;
+var mapSocketIdToken = {};
+var socketList = [];
+var mapFriendOnline = {}; //store all friend online of user
+var mapMissCall = {};// luu cac cuoc goi nho cua user
+var mapMissMsg = {}; // luu cac tin nhan lo cua user
+
+function addNewUser(userId,userType,socketId){
+  var isFound = false;
+  for(var i = 0;i < userOnlineList.length;i++){
+    var user = userOnlineList[i];
+    if((user.userId == userId && user.userType == userType) || user.socketId == socketId){
+      isFound = true;
+      user.userId = userId;
+      user.userType = userType;
+      user.socketId = socketId;
+    }
+  }
+  if(!isFound){
+    var user = {userId:userId,userType:userType,socketId:socketId};
+    userOnlineList.push(user);
+  }
+}
+
+function getUserBySocketId(socketId){
+  for(var i = 0;i < userOnlineList.length;i++){
+    if(userOnlineList[i].socketId == socketId){
+       return userOnlineList[i];
+    }
+  }
+
+  return null;
+}
+
+function getUserOnlineByUser(user){
+  var {userId, userType} = user
+  for(var i = 0;i < userOnlineList.length;i++){
+    if(userOnlineList[i].userId == userId && userOnlineList[i].userType == userType){
+       return userOnlineList[i];
+    }
+  }
+
+  return null;
+}
+
+function removeUserBySocket(socketId){
+  for(var i = 0;i < userOnlineList.length;i++){
+    var user = userOnlineList[i];
+    if(user.socketId == socketId){
+      userOnlineList.splice(i,1);
+      break;
+    }
+  }
+}
+
+function getListUserOnline(userFriends){
+  var lstUserOnline = [];
+  for(var i = 0;i < userFriends.length;i++){
+    for(var j= 0;j< userOnlineList.length;j++){
+      if(userOnlineList[j].userId == userFriends[i].userId &&
+            userOnlineList[j].userType == userFriends[i].userType){
+        lstUserOnline.push(userOnlineList[i]);
+      }
+    }
+  }
+  return lstUserOnline;
+}
+
+function exchange(socket,data){
+  console.log((new Date()).getTime() + 'exchange-id'+socket.id);
+  let user = data.to;
+  var userOnline = getUserOnlineByUser(user);
+  var fromUser = getUserBySocketId(socket.id)
+
+  // var to = io.sockets.connected[data.to];
+  if(userOnline && fromUser){
+    // let name = mapUserName[socket.id];
+    let name = "test";
+    io.to(userOnline.socketId).emit('exchange',{from:fromUser,sdp:data.sdp,candidate:data.candidate,name:name});
+  }
+}
+
+io.on('connection', function(socket) {
+  socket.on('join', function(data) {
+    var {userId,userType,userFriends} = data;
+    addNewUser(userId,userType,socket.id);
+    var userOnlineList = getListUserOnline(userFriends);
+    // Tra ve thua socket id
+    mapFriendOnline[userId] = userOnlineList;
+    // Thong bao cho tat ca friend
+    userOnlineList.forEach(friend => {
+      if(friend && friend.socketId){
+        io.to(friend.socketId).emit('friend-online',{userId:userId,userType:userType});
+      }else{
+        console.log('ERRROR FRIEND:'+friend.userId);
+      }
+    });
+    io.to(socket.id).emit('init-data-friend',{friendOnlineList: userOnlineList,missCallList:mapMissCall[userId],missMsgList:mapMissMsg[userId]});
+    mapMissCall[userId] = [];
+    mapMissMsg[userId] = [];
+    // if(fn){
+    //   fn(userOnlineList,mapMissCall[userId],mapMissMsg[userId]);
+    //   mapMissCall[userId] = [];
+    //   mapMissMsg[userId] = [];
+    // }
+  });
+
+  socket.on('disconnect', function() {
+    var user = getUserBySocketId(socket.id);
+    if(user){
+      var friendOnline = mapFriendOnline[user.userId];
+      if(friendOnline){
+          for(var i = 0 ;i < friendOnline.length;i++){
+            var friendOnline = friendOnline[i];
+            io.to(friendOnline.socketId).emit("friend-disconnect",user);
+          }
+      }
+    }
+    removeUserBySocket(socket.id);
+  });
+
+});
+
+function callToUser(socket,user){
+  var userOnline = getUserActiveByUser(user);
+  var fromUser = getUserBySocketId(socket.id);
+  if(fromUser){
+    if(userOnline){
+      // Truong hop user online
+      io.to(userOnline.socketId).emit("call-from",fromUser);
+    }else{
+      // Truong hop user offline
+      var missCallLst = mapMissCall[user.userId];
+      if(!missCallLst){
+        missCallLst = [];
+      }
+      missCallLst.push(fromUser);
+      io.to(socket.id).emit("offline-call-now",user);
+    }
+  }
+  
+}
+
+// inform to start call is ringing in end call
+function ringingCall(socket,user){
+    var user = getUserActiveByUser(user);
+    var socketUser = getUserBySocketId(socket.id);
+    if(user && socketUser){
+        io.to(user.socketId).emit("ringing",socketUser);
+    }
+}
+
+// function endCallTo(socket,user){
+//   var user = getUserActiveByUser(user);
+//   var socketUser = getUserBySocketId(socket.id);
+//   if(user && socketUser){
+//       io.to(user.socketId).emit("end-call-from",socketUser);
+//   }
+// }
+
+function finishCall(socket,user){
+  var user = getUserActiveByUser(user);
+  var socketUser = getUserBySocketId(socket.id);
+  if(user && socketUser){
+    io.to(user.socketId).emit("finish-call",socketUser);
+  }
+}
+
+function startCall(socket,user){
+  var user = getUserActiveByUser(user);
+  var socketUser = getUserBySocketId(socket.id);
+  if(user && socketUser){
+      io.to(user.socketId).emit("start-call",socketUser);
+  }
+}
+
+function busyNow(socket,user){
+  var user = getUserActiveByUser(user);
+  var socketUser = getUserBySocketId(socket.id);
+  if(user && socketUser){
+      io.to(user.socketId).emit("busy-now",socketUser);
+  }
+}
+
+function sendMsg(socket,data){
+  var user = getUserActiveByUser(data.user);
+  var socketUser = getUserBySocketId(socket.id);
+  if(socketUser){
+    if(user){
+      io.to(user.socketId).emit("send-msg",{user:socketUser,msg:data.msg});
+    }else{
+      var lstMsg = mapMissMsg[data.user.userId];
+      if(!lstMsg){
+        lstMsg = [];
+      }
+      lstMsg.push({user:user,msg:data.msg});
+    }
+      
+  }
+}
+
+function addNewFriend(socket,user){
+  var user = getUserActiveByUser(user);
+  var socketUser = getUserBySocketId(socket.id);
+  // Trong truong hop ban moi add online thi gui phan hoi lai
+  if(user && socketUser){
+    //add them vao danh sach ban be
+    var tmpListOnline = mapFriendOnline[user.userId];
+    tmpListOnline.push(socketUser);
+    mapFriendOnline[user.userId] = tmpListOnline;
+
+    tmpListOnline = mapFriendOnline[socketUser.userId];
+    tmpListOnline.push(user);
+    mapFriendOnline[socketUser.usreId] = mapFriendOnline;
+
+    io.to(user.socketId).emit('friend-online',socketUser);
+    io.to(socket.id).emit('friend-online',user);
+  }
+}
+
+// var configAuth = {authenticate:validToken,disconnect:disconnect};
+// // Filter user
+// socketioAuth(io,configAuth);
+
+var configRoom = {
+  callToUser:callToUser,
+  ringingCall:ringingCall,
+  startCall: startCall,
+  finishCall: finishCall,
+  busyNow:busyNow,
+  sendMsg:sendMsg,
+  addNewFriend: addNewFriend
+};
+socketIORoom(io,configRoom);
+
+var configRTC = {exchange:exchange};
+socketRTC(io,configRTC);
+
+
+// START THE SERVER
+// =============================================================================
+
+console.log('Magic happens on port ' + port);
---
+++ public/main.js
@@ -0,0 +1,445 @@
+
+var app = angular.module('myApp', []);
+var deviceId = Date.now();
+var SERVER_URL = 'http://localhost:3000';
+let localStream = null;
+
+window.onFriendCallback = (userId, stream) => {
+  let thumbnailElement = document.createElement("div");
+  thumbnailElement.className = "video-thumbnail";
+  thumbnailElement.id = "friend-" + userId;
+
+  let videoElement = document.createElement('video');
+  videoElement.className = "video thumbnail";
+  videoElement.autoplay = 'autoplay';
+  // try{
+  // videoElement.src = URL.createObjectURL(stream);
+  // }catch(e){
+    videoElement.srcObject = stream;
+  // }
+  thumbnailElement.appendChild(videoElement);
+
+  let nameElement = document.createElement("div");
+  nameElement.className = "name-user";
+  nameElement.innerText = userId;
+  thumbnailElement.appendChild(nameElement);
+
+  document.getElementsByClassName("videos-container")[0].appendChild(thumbnailElement);
+}
+
+window.onFriendLeft = (user) => {
+  var {userId} = user;
+  var elementId = "friend-" + userId;
+  var element = document.getElementById(elementId);
+  if(element){
+    element.parentNode.removeChild(element);
+  }
+}
+
+app.controller('myCtrl', function($scope,$http) {
+    $scope.isLogin = false;
+    // $scope.name = "a";
+    $scope.room = "ROOM_TEST";
+
+    $scope.isVideo = true;
+    $scope.isAudio = false;
+
+    $scope.calling = false;
+    $scope.makeCall = false;
+    $scope.hasNewCall = false;
+
+
+    $scope.friendAllList = [
+      {userId:'d1',active:false,userType:0,userName:'doctor 1',urlAvatar:'https://www.flaticon.com/premium-icon/icons/svg/1155/1155238.svg',friends:['p1','p2','p3']},
+      {userId:'d2',active:false,userType:0,userName:'doctor 2',urlAvatar:'https://image.flaticon.com/icons/svg/387/387561.svg',friends:['p1','p2','p3']},
+      {userId:'d3',active:false,userType:0,userName:'doctor 3',urlAvatar:'https://image.flaticon.com/icons/svg/122/122452.svg',friends:['p1','p2','p3']},
+      {userId:'p1',active:false,userType:1,userName:'Benh nhan 1',urlAvatar:'https://image.flaticon.com/icons/svg/145/145867.svg',friends:['d1','d2','d3']},
+      {userId:'p2',active:false,userType:1,userName:'Benh nhan 2',urlAvatar:'https://image.flaticon.com/icons/svg/145/145843.svg',friends:['d1','d2','d3']},
+      {userId:'p3',active:false,userType:1,userName:'Benh nhan 3',urlAvatar:'https://www.flaticon.com/premium-icon/icons/svg/145/145863.svg',friends:['d1','d2','d3']},
+    ];
+
+    $scope.filterUser = function(user){
+      for(var i = 0 ;i < $scope.friendAllList.length;i++){
+        var friend = $scope.friendAllList[i];
+        if(friend.userId == $scope.userId && friend.userType == $scope.userType){
+          if(friend.friends){
+            for(var j = 0 ;j < friend.friends.length;j++){
+              var f = friend.friends[j];
+              if(user.userId == f){
+                return true;
+              }
+            }
+          }
+        }
+      }
+
+      return false;
+    }
+
+    $scope.getUserFriends = function(){
+      var lst = [];
+      $scope.friendAllList.forEach(friend=>{
+        if(friend.userId == $scope.userId && friend.userType == $scope.userType){
+          if(friend.friends){
+            friend.friends.forEach(f=>{
+
+              $scope.friendAllList.forEach(ff =>{
+                if(ff.userId == f){
+                  lst.push(ff);
+                }
+              })
+              
+
+          })
+          }
+          
+        }  
+      });
+      return lst;
+    }
+
+    $scope.isChat = false;
+    $scope.currentChat = null; //User dang chat
+    $scope.currentListMsg = [];
+    $scope.mapAllMsg = {};
+    $scope.textInput = "";
+
+    $scope.userId = "";
+    $scope.userType = "0";
+
+    $scope.newFriend = null;
+    $scope.addNewFriend = function(){
+      if($scope.newFriend.userId == null){
+        alert('Insert new friend user id');
+      }else{
+        var newUser = {userId:$scope.newFriend.userId,userType:$scope.newFriend.userType,active:false,friends:[]};
+        $scope.friendAllList.push(newUser);
+        $scope.friendAllList.forEach(friend =>{
+          if(friend.userId == $scope.userId && friend.userType == $scope.userType){
+            friend.friends.push($scope.newFriend.userId);
+          }
+        })
+        addNewFriend(newUser);
+        $scope.newFriend = null;
+      }
+    }
+    $scope.getDateDisplay = function(date){
+      return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    }
+
+    $scope.addNewMessage = function(isMe,user,text){
+      // message duoc gui tu toi
+      var msg = {text:text,date:new Date(),isMe:isMe};
+      var {userId,userType} = user;
+      var lstMsg = null;
+      if($scope.mapAllMsg[userId]){
+        lstMsg = $scope.mapAllMsg[userId];
+      }else{
+        lstMsg = [];
+      }
+      lstMsg.push(msg);
+      $scope.mapAllMsg[userId] = lstMsg;
+      if($scope.currentChat && userId == $scope.currentChat.userId && userType == $scope.currentChat.userType){
+        $scope.currentListMsg.push(msg);
+        var objDiv = document.getElementById("chat-msg-content");
+        objDiv.scrollTop = objDiv.scrollHeight;
+      }
+    }
+
+    $scope.sendMsg = function(){
+      if($scope.currentChat != null){
+        if($scope.textInput == ""){
+          alert('Input a meassage');
+        }else{
+          sendMsg({userId:$scope.currentChat.userId,userType:$scope.currentChat.userType},$scope.textInput);
+          $scope.addNewMessage(true,{userId:$scope.currentChat.userId,userType:$scope.currentChat.userType},$scope.textInput);
+          $scope.textInput = "";
+        }
+      }else{
+        alert('Choose a user to chat');
+      }
+    }
+
+    $scope.myFunct = function(keyEvent) {
+      if (keyEvent.which === 13)
+        $scope.sendMsg();
+    }
+
+    $scope.outRoom = function(){
+      $scope.isLogin = false;
+    }
+
+    $scope.switchAudio = function(){
+        $scope.isAudio = !$scope.isAudio;
+        loadLocalStream(false,$scope.isAudio,$scope.isVideo);
+    }
+
+    $scope.switchVideo = function(){
+        $scope.isVideo = !$scope.isVideo;
+        loadLocalStream(false,$scope.isAudio,$scope.isVideo);
+    }
+
+    var peerConnections = {}; //map of {socketId: socket.io id, RTCPeerConnection}
+    var remoteViewContainer = document.getElementById("remoteViewContainer");
+
+    $scope.friends = []; //list of {socketId, name}
+    let me = null; //{socketId, name}
+
+    function receiveCallCB(stream){
+      $scope.calling = true;
+      $scope.hasNewCall = false;
+      receiveCall($scope.friendCall)
+    }
+    $scope.receiveCall = function(){  
+        loadLocalStream(false,$scope.isAudio,$scope.isVideo,receiveCallCB);
+    }
+
+    $scope.token = null;
+    $scope.getFriendOnlineCB = function(friendOnlineList,missCallList,missMsgList){
+      let userFriends = $scope.getUserFriends();
+      for(var i = 0;i < friendOnlineList.length;i++){
+        for(var j = 0; j < userFriends.length;j++){
+          if(userFriends[j].userId == friendOnlineList[i].userId &&
+              userFriends[j].userType == friendOnlineList[i].userType){
+              userFriends[j].active = true;
+          }
+        }
+      }
+      $scope.$apply();
+
+      if(missCallList && missCallList.length > 0){
+        alert('You have miss call:'+missCallList.length);
+      }
+
+      if(missMsgList && missMsgList.length > 0){
+        alert('You have miss msg:'+missMsgList.length);
+      }
+    }
+
+    $scope.newFriendOnlineCB = function(user){
+      var isFound = false;
+      for(var i = 0;i < $scope.friendAllList.length;i++){
+        if($scope.friendAllList[i].userId == user.userId
+         && $scope.friendAllList[i].userType == user.userType){
+
+          isFound = true;
+          $scope.friendAllList[i].active = true;
+        }
+
+        if($scope.friendAllList[i].userId == $scope.userId
+          && $scope.friendAllList[i].userType == $scope.userType){
+ 
+           var friendList= $scope.friendAllList[i].friends;
+           var foundFriend = false;
+           for(var j = 0 ;j < friendList.length;j++){
+             if(friendList[j] == user.userId){
+              foundFriend = true;
+             }
+           }
+           if(!foundFriend){
+            friendList.push(user.userId);
+           }
+         }
+      }
+      if(!isFound){
+        $scope.friendAllList.push({userId:user.userId,userType:user.userType,active:true});
+      }
+
+
+      $scope.$apply();
+    }
+
+    $scope.disconectFriend = function(user){
+      let {userId,userType} = user;
+
+      if($scope.friendCall && $scope.friendCall.userId == userId && $scope.friendCall.userType == userType){
+        if($scope.calling === true){
+          $scope.calling = false;
+          leave(userId);
+        }
+
+        $scope.makeCall = false;
+        $scope.hasNewCall = false;
+      }
+
+      for(var i = 0;i < $scope.friendAllList.length;i++){
+        if($scope.friendAllList[i].userId == userId &&
+            $scope.friendAllList[i].userType == userType){
+          $scope.friendAllList[i].active = false;
+        }
+      }
+
+      $scope.$apply();
+    }
+
+    $scope.startCallSuccessCB = function(user,stream){
+      console.log('start call success');
+      $scope.calling = true;
+      $scope.makeCall = false;
+      $scope.$apply();
+      onFriendCallback(user.userId,stream);
+    }
+
+    $scope.endCallCB = function(user){
+      endCallTo(user);
+      $scope.$apply();
+    }
+
+    $scope.onNewMsgCB = function(user,msg){
+      $scope.addNewMessage(false,user,msg);
+      $scope.$apply();
+    }
+
+    $scope.onNewCallCB = function(user){
+      console.log('New Call From'+user);
+      if($scope.calling === true || $scope.makeCall === true || $scope.hasNewCall === true){
+          socket.emit('busy-now',user);
+      }else{
+        $scope.hasNewCall = true;
+        $scope.friendCall = user;
+        socket.emit('ringing',user);
+        $scope.$apply();
+      }
+    }
+
+    $scope.onBusyCallCB = function(user){
+      $scope.calling = false;
+      $scope.makeCall = false;
+      $scope.friendCall = null;
+      $scope.statusCall = '';
+      setLocalStream(null);
+      alert('User Busy Now');
+      $scope.$apply();
+    }
+
+    $scope.offlineCallNow = function(user){
+      $scope.calling = false;
+      $scope.makeCall = false;
+      $scope.friendCall = null;
+      $scope.statusCall = '';
+      setLocalStream(null);
+      alert('User Not Online Now');
+      $scope.$apply();
+    }
+
+    $scope.join = function() {
+      var isFound = false;
+      for(var i = 0;i < $scope.friendAllList.length;i++){
+        if($scope.friendAllList[i].userId == $scope.userId &&
+              $scope.friendAllList[i].userType == $scope.userType){
+              isFound = true;
+        }
+      }
+      let userFriends = $scope.getUserFriends();
+      if(!isFound){
+        var newUser = {userId: $scope.userId,userType: $scope.userType,friends:[]};
+        $scope.friendAllList.push(newUser);
+      }
+
+      $scope.isLogin = true;
+      let config = {'socketURL':'http://localhost:3000'};
+        let user = {userId:$scope.userId,userType:$scope.userType,userFriends:userFriends};
+        let callback = {
+          getFriendOnlineCB:$scope.getFriendOnlineCB,//Khi co danh sach ban online tra ve
+          newFriendOnlineCB:$scope.newFriendOnlineCB,//khi co them 1 user online
+          disconectFriend:$scope.disconectFriend,// khi co 1 user disconect
+          startCallSuccessCB:$scope.startCallSuccessCB,// khi start call success
+          endCallCB:$scope.endCallCB,// Khi yeu cau ket thuc cuoc goi
+          onNewCallCB:$scope.onNewCallCB,// khi co cuoc goi moi
+          onNewMsgCB:$scope.onNewMsgCB,// khi co tin nhan moi
+          onBusyCallCB:$scope.onBusyCallCB,
+          offlineCallNow: $scope.offlineCallNow
+        };
+        createSocketRTC(config,user,callback);
+    }
+
+    $scope.callFriend = function(friend){
+      let {userId,active} = friend;
+      if($scope.calling){
+        alert('You are calling');
+      }else{
+          $scope.isChat =false;
+          $scope.calling = true;
+          $scope.makeCall = true;
+          $scope.friendCall = friend;
+          $scope.statusCall = 'Connecting';
+          // $scope.$apply();
+          loadLocalStream(false,$scope.isAudio,$scope.isVideo,(localStream)=>{
+              var user = {userId:friend.userId,userType:friend.userType};
+              makeCall(user,localStream);
+          });
+      }
+    }
+
+    $scope.chatFriend = function(friend){
+      let {userId,active} = friend;
+      $scope.isChat =true;
+      $scope.currentChat = friend;
+      if($scope.mapAllMsg[userId]){
+          $scope.currentListMsg = $scope.mapAllMsg[userId];
+      }else{
+        $scope.currentListMsg = [];
+      }
+    }
+
+    $scope.joinRoom = function(){
+      if($scope.userId == ""){
+          alert("Please insert userId!");
+      }else{
+          $scope.join($scope.room,$scope.name);
+      }
+    }
+
+    $scope.finishCall = function(){
+      finishCall($scope.friendCall);
+      endCallTo($scope.friendCall);
+    }
+
+    function endCallTo(user){
+      $scope.calling = false;
+      $scope.makeCall = false;
+      $scope.hasNewCall = false;
+      setLocalStream(null);
+      leave(user);
+    }
+
+    function loadLocalStream(muted,isAudio,isVideo,callback) {
+      navigator.getUserMedia = (
+          navigator.getUserMedia ||
+          navigator.webkitGetUserMedia ||
+          navigator.mozGetUserMedia ||
+          navigator.msGetUserMedia
+      );
+
+      URL = URL || webkitURL;
+
+      if(isAudio || isVideo){
+        navigator.getUserMedia({ "audio": isAudio, "video": isVideo }, function (stream) {
+          // console.log((new Date()).getTime() + 'loadLocalStream');
+          setLocalStream(stream);
+          if(callback){
+              callback(stream);
+          }
+        }, function(error){
+            alert(error);
+        });
+      }else{
+          setLocalStream(null);
+      }
+    }
+});
+
+function setLocalStream(stream){
+  changeLocalStream(stream);
+  localStream = stream;
+  var selfView = document.getElementById("selfView");
+  selfView.srcObject = stream;
+}
+
+function leave(user) {
+  console.log('leave', user);
+  if(onFriendLeft != null) {
+    onFriendLeft(user);
+  }
+
+}
